3c44d1af762f076701297f95634d581830542c89
noah <m3252@naver.com>
2022-06-09 00:31:20 +0900

    refactor : AppConfig 역할이 잘 보이도록 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,13 +12,32 @@ import hello.core.order.OrderServiceImpl;
 //TODO spring 의 DI 역할을 대신 해주는 느낌인데..
 public class AppConfig {
 
-    public MemberService memberService() {
+    // 무슨 일을 하는지 잘 드러나지 않는다.
+    /*public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }*/
+
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
     }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
 
 
 }
