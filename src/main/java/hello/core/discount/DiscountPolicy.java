e9537952219fedd8cf7cc8f81a3272b38bee65bc
noah <m3252@naver.com>
2022-06-08 23:31:15 +0900

    feat : 할인 정책 추가

---
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @param memeber
+     * @param price
+     * @return 할인 대상 금액
+     */
+    int discount(Member memeber, int price);
+}
